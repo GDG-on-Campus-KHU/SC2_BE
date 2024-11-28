774ec991d3b8faac7e79d71f83500d4542640717
Jihyeon An <jh011007@naver.com>
2024-11-28 21:21:48 +0900

    [FEAT] JSON 파일 경로 환경 변수에 추가

--- config/config.go
+++ config/config.go
@@ -3,16 +3,25 @@ package config
 import (
 	"context"
 	firebase "firebase.google.com/go/v4"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 	"log"
+	"os"
+)
+
+var (
+	BaseURL       string
+	ServiceKey    string
+	JSONFileRoute string
 )
 
 var FirebaseApp *firebase.App
 
 // Firebase 초기화
 func InitFirebase() {
+	JSONFileRoute = os.Getenv("JSON_FILE_ROUTE")
 	// 서비스 계정 JSON 파일 경로
-	opt := option.WithCredentialsFile("C:\\Users\\user\\Downloads\\khu-gdg-sc2-firebase-adminsdk-d60ps-1ca08cefb6.json")
+	opt := option.WithCredentialsFile(JSONFileRoute)
 
 	// Firebase 앱 초기화
 	app, err := firebase.NewApp(context.Background(), nil, opt)
@@ -21,4 +30,20 @@ func InitFirebase() {
 	}
 
 	FirebaseApp = app
+}
+
+// Initialize 환경 변수 로드 및 기본 설정 초기화
+func InitEnv() {
+	// .env 파일 로드
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found. Using system environment variables.")
+	}
+
+	// 환경 변수 로드
+	BaseURL = os.Getenv("BASE_URL")
+	ServiceKey = os.Getenv("SERVICE_KEY")
+
+	if BaseURL == "" || ServiceKey == "" {
+		log.Fatalf("BASE_URL or SERVICE_KEY is not set in environment variables")
+	}
 }
